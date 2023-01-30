4f50ecd9e044c5f9e86248603a8ae309a0274405
BuildTools <unconfigured@null.spigotmc.org>
2023-01-30 17:02:20 +0100

    second commit

---
+++ src/Adresse.java
@@ -0,0 +1,19 @@
+public class Adresse {
+    private String numero;
+    private String voie;
+    private String ville;
+    private String cp;
+
+    public Adresse(String numero, String voie, String ville, String cp){
+        this.numero = numero;
+        this.voie = voie;
+        this.ville = ville;
+        this.cp = cp;
+    }
+
+    public boolean validerChaine(String chaine, int taille){
+    return false;
+    }
+
+
+}
---
+++ src/Deposant.java
@@ -0,0 +1,11 @@
+public class Deposant {
+    private String civilite;
+    private String mail;
+    private Adresse adresse;
+
+    public Deposant(String civilite, String mail, Adresse adresse){
+        this.civilite = civilite;
+        this.mail = mail;
+        this.adresse = adresse;
+    }
+}
